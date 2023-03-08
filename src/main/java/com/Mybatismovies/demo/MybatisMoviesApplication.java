f69ef1e7b3fe0bca63e7567338e92ff6a085660d
aokiayukodesu <catch.you.later1031@gmail.com>
2023-03-08 04:36:22 -0800

    プロジェクトのセットアップ

---
+++ src/main/java/com/Mybatismovies/demo/MybatisMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.Mybatismovies.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisMoviesApplication.class, args);
+	}
+
+}
