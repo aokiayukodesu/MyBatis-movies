91045f800aade079d0388bf7b0dca83ac6273977
aokiayukodesu <catch.you.later1031@gmail.com>
2023-03-08 05:21:36 -0800

    データベースの構築

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -3,9 +3,11 @@ DROP TABLE IF EXISTS movies;
 CREATE TABLE movies (
   id int unsigned AUTO_INCREMENT,
   name VARCHAR(100) NOT NULL,
-  director VARCHAR(100) NOT NULL,
+  releaseDate VARCHAR(100) NOT NULL,
   PRIMARY KEY(id)
 );
 
-INSERT INTO movies (name, director) VALUES ("ショーシャンクの空に", "フランク・ダラボン");
-INSERT INTO movies (name, director) VALUES ("この世界の片隅に", "片渕須直");
+INSERT INTO movies (name, releaseDate) VALUES ("アルマゲドン", "2000年");
+INSERT INTO movies (name, releaseDate) VALUES ("トイ・ストーリー", "2000年");
+INSERT INTO movies (name, releaseDate) VALUES ("ファイト・クラブ", "1999年");
+INSERT INTO movies (name, releaseDate) VALUES ("セブン", "1996年");
