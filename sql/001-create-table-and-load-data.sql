dd8162aea2f7096d6b0528be5e81f7c0c6f71783
aokiayukodesu <catch.you.later1031@gmail.com>
2023-03-09 07:26:23 -0800

    カラム名と型を修正

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -3,11 +3,11 @@ DROP TABLE IF EXISTS movies;
 CREATE TABLE movies (
   id int unsigned AUTO_INCREMENT,
   name VARCHAR(100) NOT NULL,
-  releaseDate VARCHAR(100) NOT NULL,
+  releaseYear YEAR NOT NULL,
   PRIMARY KEY(id)
 );
 
-INSERT INTO movies (name, releaseDate) VALUES ("アルマゲドン", "2000年");
-INSERT INTO movies (name, releaseDate) VALUES ("トイ・ストーリー", "2000年");
-INSERT INTO movies (name, releaseDate) VALUES ("ファイト・クラブ", "1999年");
-INSERT INTO movies (name, releaseDate) VALUES ("セブン", "1996年");
+INSERT INTO movies (name, releaseYear) VALUES ("アルマゲドン", 2000);
+INSERT INTO movies (name, releaseYear) VALUES ("トイ・ストーリー", 2000);
+INSERT INTO movies (name, releaseYear) VALUES ("ファイト・クラブ", 1999);
+INSERT INTO movies (name, releaseYear) VALUES ("セブン", 1996);
