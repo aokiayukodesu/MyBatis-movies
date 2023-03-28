cd0f8f46af4b6f187835dda49a86b898c0c285d9
aokiayukodesu <catch.you.later1031@gmail.com>
2023-03-28 07:02:58 -0700

    クエリパラメーターの値を受け取る実装

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -7,7 +7,14 @@ CREATE TABLE movies (
   PRIMARY KEY(id)
 );
 
+<<<<<<< HEAD
 INSERT INTO movies (name, publishedYear) VALUES ("アルマゲドン", 2000);
 INSERT INTO movies (name, publishedYear) VALUES ("トイ・ストーリー", 2000);
 INSERT INTO movies (name, publishedYear) VALUES ("ファイト・クラブ", 1999);
 INSERT INTO movies (name, publishedYear) VALUES ("セブン", 1996);
+=======
+INSERT INTO movies (name, releaseYear) VALUES ("アルマゲドン", 2000);
+INSERT INTO movies (name, releaseYear) VALUES ("トイ・ストーリー", 2000);
+INSERT INTO movies (name, releaseYear) VALUES ("ファイト・クラブ", 1999);
+INSERT INTO movies (name, releaseYear) VALUES ("セブン", 1996);
+>>>>>>> parent of 6ebeac7 (確認して頂くために一旦データをプッシュ)
